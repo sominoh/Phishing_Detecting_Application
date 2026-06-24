43732a01c1b2c6249fc09570240148ed7e1b2edc
오소민 <145252436+sominoh@users.noreply.github.com>
2026-06-24 14:43:36 +0900

    프로젝트 디렉토리명 변경

---
+++ android/Phishing_Detecting/app/src/main/java/com/example/phishing_detecting/api/AnalysisRequest.java
@@ -0,0 +1,10 @@
+package com.example.phishing_detecting.api;
+
+public class AnalysisRequest {
+    public String text;
+
+    public AnalysisRequest(String text) {
+        this.text = text;
+    }
+}
+
